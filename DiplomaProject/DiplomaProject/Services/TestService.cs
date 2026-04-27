fb58e431ddb7da2bf82a8ba527fbe7f0efd70f39
Oleksandr Vorontsov <advorontsov004@gmail.com>
2026-04-27 19:37:19 +0300

    Optimized "TestPage" code, buttons do not move anymore

--- DiplomaProject/DiplomaProject/Services/TestService.cs
+++ DiplomaProject/DiplomaProject/Services/TestService.cs
@@ -37,37 +37,63 @@ namespace DiplomaProject.Services
             return new List<TestInfo>
     {
         new TestInfo(
-            "2 + 2 = ?",
-            new List<string> { "3", "4", "5" },
+            "3 + 5 = ?",
+            new List<string> { "5", "8", "10", "15" },
             new List<int> { 1 }
         ),
 
         new TestInfo(
-            "Capital of Ukraine?",
-            new List<string> { "Kyiv", "Lviv", "Odesa" },
-            new List<int> { 0 }
+            "Select programming languages",
+            new List<string> { "C#", "HTML", "Python", "CSS" },
+            new List<int> { 0, 2 }
         ),
 
         new TestInfo(
-            "5 * 5 = ?",
-            new List<string> { "10", "20", "25" },
+            "Which are planets?",
+            new List<string> { "Mars", "Moon", "Earth", "Sun" },
+            new List<int> { 0, 2 }
+        ),
+
+        new TestInfo(
+            "10 / 2 = ?",
+            new List<string> { "2", "3", "5", "10" },
             new List<int> { 2 }
         ),
 
         new TestInfo(
-            "C# is?",
-            new List<string> { "Language", "Database", "OS" },
-            new List<int> { 0 }
+            "Select even numbers",
+            new List<string> { "1", "2", "3", "4", "6" },
+            new List<int> { 1, 3, 4 }
         ),
-         new TestInfo(
-            "Find fruits",
-            new List<string> { "apple", "cabbage", "peach" },
-            new List<int> { 0, 2 }
+
+        new TestInfo(
+            "Which are web technologies?",
+            new List<string> { "HTTP", "FTP", "C++", "HTML", "TCP" },
+            new List<int> { 0, 1, 3, 4 }
         ),
-          new TestInfo(
-            "Find round objects",
-            new List<string> { "wheel", "plate", "cabbage" },
-            new List<int> { 0, 1 }
+
+        new TestInfo(
+            "Square root of 16?",
+            new List<string> { "2", "4", "8" },
+            new List<int> { 1 }
+        ),
+
+        new TestInfo(
+            "Select animals",
+            new List<string> { "Dog", "Car", "Cat", "Table", "Bird" },
+            new List<int> { 0, 2, 4 }
+        ),
+
+        new TestInfo(
+            "Which are databases?",
+            new List<string> { "MySQL", "PostgreSQL", "React", "MongoDB" },
+            new List<int> { 0, 1, 3 }
+        ),
+
+        new TestInfo(
+            "7 * 6 = ?",
+            new List<string> { "36", "42", "48", "56" },
+            new List<int> { 1 }
         )
     };
         }
